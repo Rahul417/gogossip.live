5a722ab99241ff3dee5c9e305cd0cc746ed712c7
Rahul Bhatt <rahul@froggydreamz.com>
2020-04-29 18:47:04 +0530

    Webhook sample code

---
+++ webhook.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Post DATA */
+$data = json_decode($_POST);
+
+// When you receive a message an RECEIVED or IMAGE type is created
+if($data->type == "RECEIVED" || $data->type == "IMAGE")
+{
+  // Default answer
+  $my_answer = "Default response is You (". $data->from .") wrote: ". $data->msg;
+
+  $response = new StdClass();
+  $response->status = "Success";
+  $response->wb_reply = $my_answer; // Attribute "wb_reply" is very important!
+
+  echo json_encode($response); // Don't forget to reply in JSON format
+
+}elseif ($data->event=="SENT") {
+
+  /* Here, you can update your data for message sent successfully. */
+
+}elseif ($data->event=="FAILED") {
+
+  /* Here, you can update your data for message failed. */
+
+}
+
+?>
